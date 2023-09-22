31b6213b01d881acf462faa0e4a5993b70fc3167
Matthew Alvarez <mattalvarez354@yahoo.com>
2023-09-22 18:53:43 -0400

    reverted function back to pass all test

--- isValidSubsequence.js
+++ isValidSubsequence.js
@@ -1,9 +1,9 @@
 function isValidSubsequence(array, sequence) {
   let index = 0;
   let seqIndx = 0;
-  //   if (array.length !== 0 && sequence.length === 0) {
-  //     return false;
-  //   }
+  if (array.length !== 0 && sequence.length === 0) {
+    return false;
+  }
 
   while (index < array.length && seqIndx < sequence.length) {
     if (array[index] === sequence[seqIndx]) {
